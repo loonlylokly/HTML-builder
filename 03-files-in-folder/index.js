11e6bb23b8443c5e3378451ca68dbeac9efe6014
loonlylokly <krivdinm@ya.ru>
2022-11-08 04:18:14 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const { readdir } = require("fs/promises");
+const { stat } = require("fs");
+const { stdout } = require("process");
+const path = require("path");
+
+async function dirInfo() {
+  let filename = path.join(path.dirname(__dirname),'03-files-in-folder/secret-folder');
+  try {
+    const files = await readdir(filename);
+    for (const file of files) {
+      stat(path.join(path.dirname(filename), "/secret-folder/"+file), (err, stats) => {
+        if (!stats.isDirectory()) {
+          stdout.write(file + " - " + path.extname(file) + " - " + stats.size + "b\n");
+        }
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+dirInfo();
